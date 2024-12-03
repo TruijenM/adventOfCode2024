917772022f280ffd4bcd0a3c1dc50f542190c376
MichielTr <r0832449@student.thomasmore.be>
2024-12-03 20:58:24 +0100

    refactor

--- src/advent/Day2.java
+++ src/advent/Day2.java
@@ -3,43 +3,46 @@ package advent;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day2 {
     List<String> file = Files.readAllLines(Paths.get("src/advent/reactorData"));
+    boolean isAscending;
+    boolean isSafeLine;
+
 
     public Day2() throws IOException {
     }
 
     public boolean isSafe(String line) {
-        String[] parts = line.split(" ");
-        int[] numbers = Arrays.stream(parts).mapToInt(Integer::parseInt).toArray();
-        return isValid(numbers) && getDifference(numbers);
+        isSafeLine = true;
+        List<String> parts = List.of(line.split(" "));
+        List<Integer> numbers = new ArrayList<>();
+        for (String part : parts) {
+            numbers.add(Integer.parseInt(part));
+        }
+        isAscending = numbers.get(0) < numbers.get(1);
+        for (int i = 1; i < numbers.size(); i++) {
+            if (!isSafeLine) return false;
+            int num1 = numbers.get(i - 1);
+            int num2 = numbers.get(i);
+            checkDifference(num1, num2);
+            checkAscending(num1, num2);
+
+        }
+        return isSafeLine;
     }
 
-    boolean getDifference(int[] numbers) {
-        for (int i = 0; i < numbers.length - 1; i++) {
-            int tempResult = Math.abs(numbers[i] - numbers[i + 1]);
-            if (!(tempResult > 0 && tempResult < 4)) return false;
-        }
-        return true;
+    void checkDifference(int num1, int num2) {
+        int tempResult = Math.abs(num1 - num2);
+        if (!(tempResult > 0 && tempResult < 4)) isSafeLine = false;
     }
 
-    boolean isValid(int[] numbers) {
-        if (numbers[0] == numbers[1]) return false;
-        boolean ascending = (numbers[0] < numbers[1]);
-        if (ascending) {
-            for (int i = 0; i < numbers.length - 1; i++) {
-                if (!(numbers[i] < numbers[i + 1])) return false;
-            }
-        }
-        if (!ascending) {
-            for (int i = 0; i < numbers.length - 1; i++) {
-                if (!(numbers[i] > numbers[i + 1])) return false;
-            }
-        }
-        return true;
+    void checkAscending(int num1, int num2) {
+        if (num1 == num2) isSafeLine = false;
+        if (isAscending && num1 > num2) isSafeLine = false;
+        if (!isAscending && num1 < num2) isSafeLine = false;
     }
 
     public int numberOfSafe() {
